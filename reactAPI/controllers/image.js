27bf35a9b49a810a66d8e6d6f0fbf6de03dc02ff
MitchMD33 <brown.l.mitchell@outlook.com>
2024-07-13 12:02:15 +0800

    Updated

---
+++ reactAPI/controllers/image.js
@@ -0,0 +1,88 @@
+
+
+const handleApiCall = (req, res) => {
+
+
+const PAT = '657e4f1eae86452aa567d50247067239';
+const USER_ID = 'clarifai';
+const APP_ID = 'main';
+const MODEL_ID = 'face-detection';
+const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
+const IMAGE_URL = req.body.input;
+const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
+
+const stub = ClarifaiStub.grpc();
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key " + PAT);
+
+
+
+stub.PostModelOutputs(
+    {
+        user_app_id: {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        model_id: MODEL_ID,
+        version_id: MODEL_VERSION_ID, // This is optional. Defaults to the latest model version
+        inputs: [
+            {
+                data: {
+                    image: {
+                        url: IMAGE_URL,
+                        // base64: imageBytes,
+                        allow_duplicate_url: true
+                    }
+                }
+            }
+        ]
+    },
+    metadata,
+    (err, response) => {
+        if (err) {
+            throw new Error(err);
+        }
+
+        if (response.status.code !== 10000) {
+            throw new Error("Post model outputs failed, status: " + response.status.description);
+        }
+
+        const regions = response.outputs[0].data.regions;
+
+        regions.forEach(region => {
+            // Accessing and rounding the bounding box values
+            const boundingBox = region.region_info.bounding_box;
+            const topRow = boundingBox.top_row.toFixed(3);
+            const leftCol = boundingBox.left_col.toFixed(3);
+            const bottomRow = boundingBox.bottom_row.toFixed(3);
+            const rightCol = boundingBox.right_col.toFixed(3);
+
+            region.data.concepts.forEach(concept => {
+                // Accessing and rounding the concept value
+                const name = concept.name;
+                const value = concept.value.toFixed(4);
+                console.log(`${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`);
+
+            });
+        });
+    }
+
+);
+
+}
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0].entries);
+  })
+  .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+  handleImage,
+  handleApiCall
+}
--- reactAPI/script.js
+++ reactAPI/script.js
@@ -2,7 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex') 
+const knex = require('knex');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+
+
 
 const db = knex({
   client: 'pg',
@@ -18,94 +25,20 @@ const db = knex({
 
 
 const app = express();
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors())
-
-
 app.get('/', (req, res) => {
-  res.send('success');
-
-})
-
-app.use(express.json());
-
-//For the login page
-app.post('/signin', (req, res) => {
-  db.select('email', 'hash').from('login')
-  .where('email', '=', req.body.email)
-  .then(data => {
-   const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-   console.log(isValid);
-   if (isValid) {
-    db.select('*').from('users')
-    .where('email', '=', req.body.email)
-    .then(user => {
-      res.json(user[0])
-    })
-    .catch(err => res.status(400).json('unable to get user')) 
-  } else {
-    res.status(400).json('wrong credentials')
-  }
-
-})
-.catch(err => res.status(400).json('wrong credentials')) 
-})
-
-
-//push new user to the database 
-app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-      trx.insert({
-        hash: hash,
-        email: email
-      })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-          .returning('*')
-          .insert({
-            email: loginEmail[0].email,
-            name: name,
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0]);
-          })
-      })
-      .then(trx.commit)
-      .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'))
-})
+  res.send(database.users);})
 
 
 
-app.get('/profile/:id', (req, res) => {
-  const { id } = req.params;
-  db.select('*').from('users').where({id})
-    .then ( users => {
-      if (users.length) {
-        res.json(users[0])
-      } else {
-        res.status(400).json('Not found')
-      }
-    })
-    .catch(err => res.status(400).json('error getting user'))
-})
 
-app.put('/image', (req, res) => {
-  const { id } = req.body;
-  db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0].entries);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
-})
+app.post('/signin',signin.handleSignin(db,bcrypt))
+app.post('/register',(req,res) =>{register.handleRegister(req,res,db,bcrypt)})
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)} )
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
 
 //Keeps the server runnning 
 app.listen(3000, () => {
